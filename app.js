8df56da39b370e44f808ecdaad510518c705a2d5
Nick Algarra <algarran24@gmail.com>
2020-02-27 18:37:14 -0600

    added content to app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+app.use(express.static('assets'));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+app.get("/api/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post('/api/notes', (req, res) => {
+    
+    const data = req.body;
+    const id = Date.now();
+    const db = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+    
+    const note = {
+      id: id,
+      title: data.title,
+      text: data.text,
+    };
+    const dbObj = JSON.parse(db);
+
+    dbObj.push(note);
+
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(dbObj), 'utf8');
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+  });
+  
+  app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const db = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+    
+    const dbObj = JSON.parse(db);
+    for (let x = 0; x < dbObj.length; x++) {
+      if (dbObj[x].id.toString() === id) {
+        dbObj.splice(x, 1);
+        break;
+      }
+    }
+    
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(dbObj), 'utf8');
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+  });
+
+
+
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT)
+});
