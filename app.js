0e239dcd939b9cbd53e85f5fd97a64e0caa54df6
Nick Algarra <algarran24@gmail.com>
2020-02-29 13:12:57 -0600

    finally debugged the index.js by moving it into the assests folder, created db.json

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-app.use(express.static('assets'));
+app.use(express.static("assets"));
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,50 +11,50 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./index.html"));
+    response.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./notes.html"));
+    response.sendFile(path.join(__dirname, "./notes.html"));
 });
 
 app.get("/api/notes", (req,res) => {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    response.sendFile(path.join(__dirname, "/db.json"));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
     
     const data = req.body;
     const id = Date.now();
-    const db = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+    const db = fs.readFileSync(path.join(__dirname, "/db.json"), "utf8");
     
     const note = {
       id: id,
       title: data.title,
       text: data.text,
     };
-    const dbObj = JSON.parse(db);
 
-    dbObj.push(note);
+    const database = JSON.parse(db)
 
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(dbObj), 'utf8');
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    database.push(note);
+    fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(database), "utf8");
+    response.sendFile(path.join(__dirname, "/db.json"));
   });
   
-  app.delete('/api/notes/:id', (req, res) => {
+  app.delete("/api/notes/:id", (req, res) => {
     const id = req.params.id;
-    const db = fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8');
+    const db = fs.readFileSync(path.join(__dirname, "/db.json"), "utf8");
     
-    const dbObj = JSON.parse(db);
-    for (let x = 0; x < dbObj.length; x++) {
-      if (dbObj[x].id.toString() === id) {
-        dbObj.splice(x, 1);
+    const database = JSON.parse(db);
+    for (let x = 0; x < database.length; x++) {
+      if (database[x].id.toString() === id) {
+        database.splice(x, 1);
         break;
       }
     }
     
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(dbObj), 'utf8');
-    res.sendFile(path.join(__dirname, '/db/db.json'));
+    fs.writeFileSync(path.join(__dirname, "/db.json"), JSON.stringify(database), "utf8");
+    response.sendFile(path.join(__dirname, "/db.json"));
   });
 
 
